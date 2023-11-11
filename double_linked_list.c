62695ef4bd1df75de9bc7d999d8cc8c10cc8134c
Pavan-Mahesh <150039478+Pavan-Mahesh@users.noreply.github.com>
2023-11-11 09:37:49 +0530

    File added

--- double_linked_list.c
+++ double_linked_list.c
@@ -14,8 +14,9 @@ void search();
 int length();
 
 typedef struct node {
+    struct node * prev;
     int data;
-    struct node * link;
+    struct node * next;
 }Node;
 
 Node * head = NULL;
@@ -25,13 +26,70 @@ int main() {
     return 0;
 }
 
-int length() {}
+int length() {
+    if(head == NULL)
+        return 0;
+    Node * temp = head;
+    int count = 1;
+    while(temp->next != NULL) {
+        temp = temp->next;
+        count++;
+    }
+    return count;
+}
 
-void insertAtBegin() {}
+void insertAtBegin() {
+    Node * new = (Node *)malloc(sizeof(Node));
+    printf("\nEnter the value: ");
+    scanf("%d", &new->data);
+    new->prev = NULL;
+    new->next = head;
+    if(head != NULL) 
+        head->prev = new;
+    head = new;
+    printf("Node inserted\n");
+}
 
-void insertAtEnd() {}
+void insertAtEnd() {
+    if(head == NULL)
+        insertAtBegin();
+    else {
+        Node * temp = head;
+        while(temp->next != NULL) 
+            temp = temp->next;
+        Node * new = (Node *)malloc(sizeof(Node));
+        printf("\nEnter the value: ");
+        scanf("%d", &new->data);
+        new->prev = temp;
+        new->next = NULL;
+        temp->next = new;
+        printf("Node inserted\n");
+    }
+}
 
-void insertAtPosition() {}
+void insertAtPosition() {
+    int count = length();
+    int pos;
+    printf("\nEnter the position: ");
+    scanf("%d", &pos);
+    if(pos == 1)
+        insertAtBegin();
+    else if(pos == (count+1))
+        insertAtEnd();
+    else if(pos>1 && pos<=count) {
+        Node * temp = head;
+        for(int i=1; i<(pos-1); i++) 
+            temp = temp->next;
+        Node * new = (Node *)malloc(sizeof(Node));
+        printf("\nEnter the value: ");
+        scanf("%d", &new->data);
+        new->prev = temp;
+        new->next = temp->next;
+        temp->next->prev = new;
+        temp->next = new;
+        printf("Node inserted\n");
+    }
+}
 
 void deleteAtBegin() {}
 
