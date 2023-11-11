14c2b4cc35f53772d670f1c3291a2e624633af88
Pavan-Mahesh <150039478+Pavan-Mahesh@users.noreply.github.com>
2023-11-11 21:43:52 +0530

    File added

--- sorting/quick_sort.c
+++ sorting/quick_sort.c
@@ -1,56 +1,53 @@
 #include <stdio.h>
 #include <conio.h>
 
-void quick_sort(int [], int, int);
-void print_array(int [], int);
+void quick_sort(int, int);
+void print_array();
 
-int len;
+int a[] = {7, 6, 10, 5, 9, 2, 1, 15, 7};
+int len = sizeof(a)/sizeof(a[0]);
 
 int main() {
-    int a[] = {7, 6, 10, 5, 9, 2, 1, 15, 7};
-    len = sizeof(a)/sizeof(a[0]);
-    
     printf("\nGiven list: ");
-    print_array(a, len);
+    print_array();
 
-    printf("===============================");
-    quick_sort(a, 0, len-1);
-    printf("===============================");
+    quick_sort(0, len-1);
 
     printf("\nSorted list: ");
-    print_array(a, len);
+    print_array();
 
     getch();
     return 0;
 }
 
-void print_array(int a[], int len) {
+void print_array() {
     printf("\n");
     for(int i=0; i<len; i++)
         printf("%d ", a[i]);
     printf("\n");
 }
 
-void quick_sort(int a[], int low, int high) {
+void swap(int p, int q) {
+    int temp = a[p];
+    a[p] = a[q];
+    a[q] = temp;
+}
+
+void quick_sort(int low, int high) {
     if(low < high) {
         int pivot = a[low], start=low, end=high;
         do_again:
-        print_array(a, len);
         while(!(a[start] > pivot) && start<high)
             start++;
         while(!(a[end] <= pivot) && end>low)
             end--;
         if(start<end) {
-            int temp = a[start];
-            a[start] = a[end];
-            a[end] = temp;
+            swap(start, end);
             goto do_again;
         } else { // start >= end
-            int temp = a[low];
-            a[low] = a[end];
-            a[end] = temp;
-            quick_sort(a, low, end-1);
-            quick_sort(a, end+1, high);
+            swap(low, end);
+            quick_sort(low, end-1);
+            quick_sort(end+1, high);
         }
     }
 }
